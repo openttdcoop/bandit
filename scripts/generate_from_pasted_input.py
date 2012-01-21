6124a9287bf9446b8b05eb41c38d530e96e47e92
andythenorth <andythenorth@localhost>
2012-01-21 22:51:13 +0000

    Change: auto-generate BANDIT truck config and lang from pasted output of web cms

---
+++ scripts/generate_from_pasted_input.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+source = open('paste_here.txt').read().split('/*-split here-*/')
+
+all_trucks = open('sprites/nml/all_trucks.pnml','w')
+all_trucks.write(source[0])
+all_trucks.close()
+
+local_strings = open('lang/english.lng.in').read()
+lang = open('lang/english.lng', 'w')
+lang.write(local_strings + source[1])
+lang.close()

ea38949bed2cc3cff89b4679d54d9f5d9e3fa85b
andythenorth <andythenorth@localhost>
2012-01-29 17:56:56 +0000

    Add: copy of the file the cms uses for rendering dicts for pasting

---
+++ misc/render_trucks_to_dicts.py
@@ -0,0 +1,29 @@
+vehicle_list = container.vehicle_storage.objectValues(['File'])
+
+vehicles_dict = {}
+
+for i in vehicle_list:
+  this_veh_props = {}
+  for j in i.propertyIds():
+    if i.getPropertyType(j) == 'date':
+      this_veh_props[j] = str(i.getProperty(j))
+    else:      
+      this_veh_props[j] = i.getProperty(j)
+  del this_veh_props['content_type']
+  this_veh_props['total_capacity'] = i.truck_capacity
+  this_veh_props['trailers_properties'] = {}
+  for j in range(0,i.truck_num_trailers):
+    trailer_props = this_veh_props['trailers_properties'][i.id() + "_trailer_" + str(j+1)] = {}
+    trailer_props['numeric_id'] = i.numeric_ID + j + 1
+    trailer_capacity = int(i.trailer_capacities[j])
+    #capacity first trailer of fifth wheel truck split with truck according to decimal ratio
+    if j == 0 and i.truck_type == "GLOBAL_TRUCK_TYPE_FIFTH_WHEEL":
+      this_veh_props['truck_capacity'] = truck_capacity = int(float(trailer_capacity) * (i.fifth_wheel_truck_capacity_fraction))
+      trailer_capacity = trailer_capacity - truck_capacity
+    trailer_props['trailer_capacity'] = trailer_capacity
+    this_veh_props['total_capacity'] = this_veh_props['total_capacity'] + trailer_capacity
+    # add graphics file, lengths etc here later
+  vehicles_dict[i.id()] = this_veh_props
+    
+print "vehicles_dict = " + str(vehicles_dict)
+return printed

bd03f0d0e95ad7571c1c0b26897689626eb1921a
andythenorth <andythenorth@localhost>
2012-01-30 23:31:43 +0000

    Fix: missing file

---
+++ sprites/nml/template_globals.py
@@ -0,0 +1,9 @@
+template_globals = {
+}
+
+standard_class_refits = {
+  'default' : {
+        'allow' : 'CC_MAIL, CC_EXPRESS, CC_ARMOURED, CC_BULK, CC_PIECE_GOODS,CC_LIQUID, CC_REFRIGERATED, CC_COVERED,',
+        'disallow' : 'CC_PASSENGERS',
+  },
+}

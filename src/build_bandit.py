01b14b9ad41449d5bf7fc4a1b231a6facb8a1618
andythenorth <andythenorth@localhost>
2012-02-04 17:29:44 +0000

    Codechange: move global_constants to a better place

--- src/build_bandit.py
+++ src/build_bandit.py
@@ -4,7 +4,7 @@ import os.path
 currentdir = os.curdir
 
 import sys
-sys.path.append(os.path.join('src','templates')) # add to the module search path
+sys.path.append(os.path.join('src')) # add to the module search path
 
 import codecs #used for writing files - more unicode friendly than standard open() module
 

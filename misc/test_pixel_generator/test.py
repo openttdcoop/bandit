d34b43f5d3aecbc985871a2cf4688b63c21a7ec5
andythenorth <andythenorth@localhost>
2012-02-18 07:15:31 +0000

    Codechange: use a stub object instead of nesting tuples and lists and such

--- misc/test_pixel_generator/test.py
+++ misc/test_pixel_generator/test.py
@@ -6,15 +6,54 @@ spritesheet = Image.open('test_input.png')
 spritesheetpx = spritesheet.load()
 draw = ImageDraw.Draw(spritesheet)
 
-# global constants
-body_colour = 10
-cc_colour = 202
 
-#each sequence is a tuple containing lists in format: [(x-offset,y-offset), colour]
-body_outer = ([(0, 0), body_colour], [(0, 1), cc_colour], [(0, 2), body_colour], [(0, 3), body_colour], [(0, 4), 13])
-body_end   = ([(0, 0), body_colour], [(0, 1), cc_colour], [(0, 2), body_colour], [(0, 3), body_colour], [(0, 4), 13])
-body_inner = ([(0, 0), 16], [(0, 1), 17], [(0, 2), 18], [(0, 3), 19], [(0, 4), 14])
-bulk_load_1 = ([(0, 2), 4],)
+class P:
+    """ simple class to hold the definition of a pixel that should be drawn """
+    def __init__(self, dx, dy, colour):
+        self.dx = dx
+        self.dy = dy
+        self.colour = colour
+
+def get_pixel_sequence(x, y, key_colour):
+    key_map = key_colour_mapping[key_colour]
+    raw_sequence = key_map['seq']
+    pixel_sequence = []
+    for P in raw_sequence:
+        pixel_sequence.append((x + P.dx, y - P.dy, P.colour + key_map['colour_shift']))
+    return pixel_sequence
+
+
+
+# global constants
+BODY = 10
+CC_A = 202
+BULK_CARGO = 4  
+
+#each sequence contains stubby objects which are constructed with params (x-offset, y-offset, colour to draw)
+body_outer = [
+    P(0, 0, BODY),
+    P(0, 1, CC_1), 
+    P(0, 2, BODY), 
+    P(0, 3, BODY), 
+    P(0, 4, 13),
+]
+body_end   = [
+    P(0, 0, BODY), 
+    P(0, 1, CC_1), 
+    P(0, 2, BODY), 
+    P(0, 3, BODY), 
+    P(0, 4, 13),
+]
+body_inner = [
+    P(0, 0, 16), 
+    P(0, 1, 17), 
+    P(0, 2, 18),
+    P(0, 3, 19), 
+    P(0, 4, 14),
+]
+bulk_load_1 = [
+    P(0, 2, BULK_CARGO),
+]
 
 key_colour_mapping = {
     209 : dict(seq = body_inner,  colour_shift =  0),
@@ -30,14 +69,6 @@ key_colour_mapping = {
     141 : dict(seq = bulk_load_1, colour_shift =  0),
 }
 
-def get_pixel_sequence(x, y, key_colour):
-    key_map = key_colour_mapping[key_colour]
-    raw_sequence = key_map['seq']
-    pixel_sequence = []
-    for (dx,dy), col in raw_sequence:
-        pixel_sequence.append((x + dx, y - dy, col + key_map['colour_shift']))
-    return pixel_sequence
-
 colours = {} #used for debug
 for x in range(spritesheet.size[0]):
   for y in range(spritesheet.size[1]):
@@ -49,6 +80,6 @@ for x in range(spritesheet.size[0]):
         for sx, sy, scol in seq:
             draw.point([(sx, sy)], fill=scol)
 
-spritesheet.save('a_test_trailer.png')
+spritesheet.save('a_test_trailer.png', optimize=1)
 
 print colours # debug: what colours did we find in this spritesheet?

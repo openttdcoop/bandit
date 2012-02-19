d1f6e0c32b23bd5699e0aaca36c1985039917147
andythenorth <andythenorth@localhost>
2012-02-19 08:47:03 +0000

    Codechange: generator handles colour variations

--- misc/pixel_generator/gestalts/tipping_trailer_4px.py
+++ misc/pixel_generator/gestalts/tipping_trailer_4px.py
@@ -23,51 +23,64 @@ load_states = [
 SPRITEROW_HEIGHT = 40
 SPRITEROW_START_Y = 10
 
-# colour onstants
-BODY = 10
-CC_A = 202
-
+# colour sets
+coloursets = {    
+    'cc_1' : dict ( 
+        body_colour = 10,
+        company_colour = 202,
+    ),
+    'cc_2' : dict ( 
+        body_colour = 84,
+        company_colour = 202,
+    ),
+}
 # pixel sequences
 # each sequence contains stubby objects which are constructed with params (x-offset, y-offset, colour to draw)
 # if colour values etc need to change for different load states etc, sequence needs to be returned from a def
 def bulk_load(cargo_colour,load_offset): 
     return [P(0, load_offset, cargo_colour)]
 
-body_outer = [
-    P(0, 0, BODY),
-    P(0, 1, CC_A), 
-    P(0, 2, BODY), 
-    P(0, 3, BODY), 
-    P(0, 4, 13),
-]
-body_end   = [
-    P(0, 0, BODY), 
-    P(0, 1, CC_A), 
-    P(0, 2, BODY), 
-    P(0, 3, BODY), 
-    P(0, 4, 13),
-]
-body_inner = [
-    P(0, 0, 16), 
-    P(0, 1, 17), 
-    P(0, 2, 18),
-    P(0, 3, 19), 
-    P(0, 4, 14),
-]
+def body_outer(colour_set):
+    c = colour_set
+    return [
+        P(0, 0, c['body_colour']),
+        P(0, 1, c['company_colour']), 
+        P(0, 2, c['body_colour']), 
+        P(0, 3, c['body_colour']), 
+        P(0, 4, 13),
+    ]
+def body_end(colour_set):
+    c = colour_set
+    return [
+        P(0, 0, c['body_colour']), 
+        P(0, 1, c['company_colour']), 
+        P(0, 2, c['body_colour']), 
+        P(0, 3, c['body_colour']), 
+        P(0, 4, 13),
+    ]
+def body_inner(colour_set):
+    c = colour_set
+    return [
+        P(0, 0, 16), 
+        P(0, 1, 17), 
+        P(0, 2, 18),
+        P(0, 3, 19), 
+        P(0, 4, 14),
+    ]
 
-def key_colour_mapping(cargo,load_state):
+def key_colour_mapping(cargo,load_state, colourset):
     if load_state[0] == 'empty':
         cargo_or_empty = [P(0, 0, 19)] 
     else:
         cargo_or_empty = bulk_load(cargo_colour=bulk_cargos[cargo],load_offset=load_state[1])    
     return {
-        209 : dict(seq = body_inner,  colour_shift =  0),
-         90 : dict(seq = body_inner,  colour_shift =  1),
-        238 : dict(seq = body_outer,  colour_shift =  0),
-        243 : dict(seq = body_outer,  colour_shift = -1),
-        244 : dict(seq = body_outer,  colour_shift =  2),
-        240 : dict(seq = body_end,    colour_shift = -1),
-        166 : dict(seq = body_end,    colour_shift =  1),
+        209 : dict(seq = body_inner(colourset),  colour_shift =  0),
+         90 : dict(seq = body_inner(colourset),  colour_shift =  1),
+        238 : dict(seq = body_outer(colourset),  colour_shift =  0),
+        243 : dict(seq = body_outer(colourset),  colour_shift = -1),
+        244 : dict(seq = body_outer(colourset),  colour_shift =  2),
+        240 : dict(seq = body_end(colourset),    colour_shift = -1),
+        166 : dict(seq = body_end(colourset),    colour_shift =  1),
         138 : dict(seq = cargo_or_empty, colour_shift = -3),
         139 : dict(seq = cargo_or_empty, colour_shift = -2),
         140 : dict(seq = cargo_or_empty, colour_shift = -1),
@@ -80,7 +93,8 @@ class Variation:
         self.spritesheets = []
 
 colour_variations = [
-    Variation(id='cc1')
+    Variation(id='cc_1'),
+    Variation(id='cc_2'),
 ]
 
 class Spritesheet:
@@ -95,17 +109,18 @@ class Spritesheet:
         self.floorplan = floorplan
         return None
         
-    def render(self):    
+    def render(self, colourset):    
         for i, load_state in enumerate(load_states):
             row = self.floorplan.copy()
-            row = render(row, key_colour_mapping(cargo=self.cid, load_state=load_state))
+            row = render(row, key_colour_mapping(cargo=self.cid, load_state=load_state, colourset=colourset))
             start_y = i * SPRITEROW_HEIGHT
             end_y = (i+1) * SPRITEROW_HEIGHT            
             self.sprites.paste(row,(0, start_y, row.size[0], end_y))    
         
-    def save(self, variation):
+    def save(self, variation_id):
         length = '7_8' # !! hard coded var until this is figured out
-        output_path = 'results/' + length + '_tipping_trailer_' + variation.id + '_' + self.cid + '.png' 
+        connection_type = 'fifth_wheel'
+        output_path = 'results/' + length + '_tipping_trailer_' + connection_type + '_' + variation_id + '_' + self.cid + '.png' 
         self.sprites.save(output_path, optimize=True)
 
 
@@ -126,5 +141,5 @@ def generate(input_image_path):
             
     for variation in colour_variations:
         for spritesheet in variation.spritesheets:
-            spritesheet.render()
-            spritesheet.save(variation)
+            spritesheet.render(coloursets[variation.id])
+            spritesheet.save(variation.id)

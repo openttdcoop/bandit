0de7315d1c55e0d4970a4c7dac42cd80dc609083
andythenorth <andythenorth@localhost>
2012-02-29 20:51:55 +0000

    Codechange: refactor colourset approach (alberth)

--- misc/pixel_generator/gestalts/flat_trailer.py
+++ misc/pixel_generator/gestalts/flat_trailer.py
@@ -20,16 +20,10 @@ SPRITEROW_HEIGHT = 40
 FLOORPLAN_START_Y = 90
 
 # colour sets
-coloursets = {
-    'cc_1' : dict (
-        deck_colour = 115,
-        company_colour = 202,
-    ),
-    'cc_2' : dict (
-        deck_colour = 75,
-        company_colour = 84,
-    ),
-}
+coloursets = [
+    ('cc_1', dict(deck_colour = 115, company_colour = 202)),
+    ('cc_2', dict(deck_colour = 75,  company_colour = 84 )),
+]
 # pixel sequences
 flatbed = [
     (0, 0, 'deck_colour'),
@@ -113,9 +107,9 @@ def hide_or_show_drawbar_dolly_wheels(connection_type):
 
 
 class Variation:
-    def __init__(self, colourset, cargo, connection_type):
-        self.id = id
+    def __init__(self, set_name, colourset, cargo, connection_type):
         self.spritesheets = []
+        self.set_name = set_name
         self.colourset = colourset
         self.cargo = cargo
         self.connection_type = connection_type
@@ -128,13 +122,13 @@ def generate(input_image_path):
     palette = Image.open('palette_key.png').palette
     # create variations containing empty spritesheets
     variations = []
-    for colourset in coloursets:
+    for set_name, colourset in coloursets:
         for cargo in cargos:
             for connection_type in ('fifth_wheel','drawbar'):
-                variation = Variation(colourset = colourset, cargo=cargo, connection_type='fifth_wheel')
+                variation = Variation(set_name = set_name, colourset = colourset, cargo=cargo, connection_type='fifth_wheel')
                 spritesheet = Spritesheet(
-                    spritesheet_width=floorplan.size[0],
-                    spritesheet_height=SPRITEROW_HEIGHT * (len(load_states)),
+                    width=floorplan.size[0],
+                    height=SPRITEROW_HEIGHT * (len(load_states)),
                     palette=palette
                 )
                 variation.spritesheets.append(spritesheet)
@@ -143,22 +137,21 @@ def generate(input_image_path):
     # render stage
     for variation in variations:
         for spritesheet in variation.spritesheets:
-            colourset = coloursets[variation.colourset]
             spriterows = []
             for load in load_states:
                 # spriterow holds data needed to render the row
                 spriterow = {'height' : SPRITEROW_HEIGHT, 'floorplan' : floorplan}
                 # add n render passes to the spriterow (list controls render order, index 0 = first pass)
                 spriterow['render_passes'] = [
-                    {'seq' : hide_or_show_drawbar_dolly_wheels(variation.connection_type), 'colourset' : colourset},
-                    {'seq' : sc_pass_1, 'colourset' : colourset},
-                    {'seq' : sc_pass_2, 'colourset' : colourset},
-                    {'seq' : sc_pass_3, 'colourset' : colourset},
-                    {'seq' : sc_pass_4, 'colourset' : colourset},
+                    {'seq' : hide_or_show_drawbar_dolly_wheels(variation.connection_type), 'colourset' : variation.colourset},
+                    {'seq' : sc_pass_1, 'colourset' : variation.colourset},
+                    {'seq' : sc_pass_2, 'colourset' : variation.colourset},
+                    {'seq' : sc_pass_3, 'colourset' : variation.colourset},
+                    {'seq' : sc_pass_4, 'colourset' : variation.colourset},
                 ]
                 spriterows.append(spriterow)
             spritesheet.render(spriterows=spriterows)
             length = '7_8' # !! hard coded var until this is figured out
-            output_path = 'results/' + length + '_flat_trailer_' + variation.connection_type + '_' + variation.colourset + '_' + variation.cargo + '.png'
+            output_path = 'results/' + length + '_flat_trailer_' + variation.connection_type + '_' + variation.set_name + '_' + variation.cargo + '.png'
             print output_path
             spritesheet.save(output_path)

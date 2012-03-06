5f0be968a0ef043cd6bf03c25edf742ddb0859db
andythenorth <andythenorth@localhost>
2012-03-06 08:27:48 +0000

    Codechange: basic support for generating a coil cargo spritesheet with recoloring

--- misc/pixel_generator/gestalts/common.py
+++ misc/pixel_generator/gestalts/common.py
@@ -23,20 +23,29 @@ def hide_or_show_drawbar_dolly_wheels(connection_type):
     )
 
 class Variation:
-    def __init__(self, set_name, colourset, cargo, connection_type, body_subtype=''):
+    def __init__(self, set_name, colourset, cargo, connection_type, length, body_subtype=''):
         self.spritesheets = []
         self.set_name = set_name
         self.colourset = colourset
         self.cargo = cargo
         self.connection_type = connection_type
+        self.length = length
         self.body_subtype = body_subtype # optional, use for e.g. flatbeds with and w/o stakes
+    
+    def attach_floorplan(self, floorplan):
+        self.floorplan = floorplan
 
-def construct_filename(gestalt_id, length, variation):
-    """ simple filename maker, not much to see here """
-    return '_'.join([length, gestalt_id, variation.connection_type, variation.set_name, variation.cargo]) + '.png'
+def construct_filename(gestalt_id, variation):
+    """ Simple filename maker; will filter out attributes that have no meaningful value. """
+    raw = [str(variation.length) + '_8', gestalt_id, variation.connection_type, variation.set_name, variation.cargo]
+    clean = []
+    for i in raw:
+        if i is not None and i != '':
+            clean.append(i)
+    return '_'.join(clean) + '.png'
 
 def get_output_path(filename):
-    """ simple path maker, to deal with OS path. Expects a filename.  Currently hard coded to use 'output' dir """
+    """ Simple path maker, to deal with OS path. Expects a filename.  Currently hard coded to use 'output' dir. """
     return os.path.join(currentdir, 'output', filename)
     
 # constants
@@ -47,3 +56,4 @@ DOS_PALETTE = Image.open('palette_key.png').palette
 CC1 = 202
 CC2 = 84
 COL_COAL = 4
+COL_MASK = 0

14f051fbfdf66504762d78d16ba4399f832a4ba4
andythenorth <andythenorth@localhost>
2012-03-07 09:32:58 +0000

    Codechange: comment in cargo generation

--- misc/pixel_generator/main.py
+++ misc/pixel_generator/main.py
@@ -10,10 +10,10 @@ from gestalts import cargo_coils
 from gestalts import tipping_trailer_4px
 from gestalts import flat_trailer
 from gestalts import tank_trailer
-"""
+
 input_image_path = os.path.join(currentdir, 'input','cargo_coils_floorplan.png')
 cargo_coils.generate(input_image_path)
-"""
+
 input_image_path = os.path.join(currentdir, 'input','test_input.png')
 tipping_trailer_4px.generate(input_image_path)
 flat_trailer.generate(input_image_path)

07bba0a0a3aa39cf66bd7a664ad163b551f5d97e
andythenorth <andythenorth@localhost>
2012-03-10 16:35:12 +0000

    Add: main.py (not the previous main.py)

---
+++ misc/pixel_generator/main.py
@@ -0,0 +1,47 @@
+print "running"
+
+import dispatcher
+from multiprocessing import Process, active_children
+
+filenames = [
+    'tipping_trailer_4px-fifth_wheel-cc1-7_8-GRAI.png',
+    'tipping_trailer_4px-fifth_wheel-cc2-7_8-GRAI.png',
+    'tipping_trailer_4px-fifth_wheel-light_grey-7_8-GRAI.png',
+    'tipping_trailer_4px-drawbar-cc1-7_8-GRAI.png',
+    'tipping_trailer_4px-drawbar-cc2-7_8-GRAI.png',
+    'tipping_trailer_4px-drawbar-light_grey-7_8-GRAI.png',
+    'tipping_trailer_4px-fifth_wheel-cc1-7_8-COAL.png',
+    'tipping_trailer_4px-fifth_wheel-cc2-7_8-COAL.png',
+    'tipping_trailer_4px-fifth_wheel-light_grey-7_8-COAL.png',
+    'tipping_trailer_4px-drawbar-cc1-7_8-COAL.png',
+    'tipping_trailer_4px-drawbar-cc2-7_8-COAL.png',
+    'tipping_trailer_4px-drawbar-light_grey-7_8-COAL.png',
+    'tipping_trailer_4px-fifth_wheel-cc1-7_8-IORE.png',
+    'tipping_trailer_4px-fifth_wheel-cc2-7_8-IORE.png',
+    'tipping_trailer_4px-fifth_wheel-light_grey-7_8-IORE.png',
+    'tipping_trailer_4px-drawbar-cc1-7_8-IORE.png',
+    'tipping_trailer_4px-drawbar-cc2-7_8-IORE.png',
+    'tipping_trailer_4px-drawbar-light_grey-7_8-IORE.png',
+    'tipping_trailer_4px-fifth_wheel-cc1-7_8-CLAY.png',
+    'tipping_trailer_4px-fifth_wheel-cc2-7_8-CLAY.png',
+    'tipping_trailer_4px-fifth_wheel-light_grey-7_8-CLAY.png',
+    'tipping_trailer_4px-drawbar-cc1-7_8-CLAY.png',
+    'tipping_trailer_4px-drawbar-cc2-7_8-CLAY.png',
+    'tipping_trailer_4px-drawbar-light_grey-7_8-CLAY.png',
+    'tipping_trailer_4px-fifth_wheel-cc1-7_8-GRVL.png',
+    'tipping_trailer_4px-fifth_wheel-cc2-7_8-GRVL.png',
+    'tipping_trailer_4px-fifth_wheel-light_grey-7_8-GRVL.png',
+    'tipping_trailer_4px-drawbar-cc1-7_8-GRVL.png',
+    'tipping_trailer_4px-drawbar-cc2-7_8-GRVL.png',
+    'tipping_trailer_4px-drawbar-light_grey-7_8-GRVL.png',
+]
+
+for filename in filenames:
+    print filename
+    Process(target=dispatcher.dispatch, args=(filename,)).start()
+
+while True:
+    if len(active_children()) == 0:
+        break
+
+print "done"

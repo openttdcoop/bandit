0ecb9e9b30a3454ef430dd87179921be1dec03da
andythenorth <andythenorth@localhost>
2012-03-17 21:31:03 +0000

    Codechange: improve deps making script for graphics

--- src/pixel_generator/get_filenames_from_nfo.py
+++ src/pixel_generator/get_filenames_from_nfo.py
@@ -1,23 +1,33 @@
 import codecs
 
-nfo = codecs.open('sprites/bandit.nfo')
+nfo = codecs.open('../../sprites/bandit.nfo')
 
-main_results = []
+main_results = set()
 for line in nfo:
     if len(line.split('.png')) > 1:
         line = 'src/' + line.partition('src/')[2]
         line = line.partition('.png')[0] + '.png'
-        main_results.append(line)
+        main_results.add(line)
 
 deps_main = codecs.open('deps_main.txt','w','ascii')
 deps_main.write('\n'.join(main_results))
 deps_main.close()
 
-graphics_results = []
+generated_results = set()
 for result in main_results:
-    if len(result.split('cargo')) > 1:
-        print 'cargo'
+    if len(result.split('output/')) > 1:
+        png = result.partition('output/')[2]
+        generated_results.add(png)
 
-deps_graphics = codecs.open('deps_graphics.txt','w','ascii')
-deps_graphics.write('\n'.join(graphics_results))
-deps_graphics.close()
+deps_generated = codecs.open('deps_generated.txt','w','ascii')
+deps_generated.write('\n'.join(generated_results))
+deps_generated.close()
+
+cargo_results = set()
+for result in generated_results:
+    if len(result.split('cargo_')) > 1:
+        cargo_results.add(result)
+
+deps_cargo = codecs.open('deps_cargo.txt','w','ascii')
+deps_cargo.write('\n'.join(cargo_results))
+deps_cargo.close()

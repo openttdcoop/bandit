40904e81dce050423ec1afcc2329781113d68044
andythenorth <andythenorth@localhost>
2012-05-13 19:42:22 +0100

    Codechange: have the quick build script also run the pixel generator (less quick :P)

--- makebandit.sh
+++ makebandit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
   ./src/build_bandit.py \
+  && make -C src/pixel_generator \
   && nmlc bandit.nml --nfo=sprites/bandit.nfo \
   && grfcodec -e bandit.grf \
   && mv bandit.grf /Users/andy/Documents/OpenTTD/data

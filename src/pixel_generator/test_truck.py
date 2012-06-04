d1292ecc5b4d73afd020c0821ebdfe4c79caf30f
andythenorth <andythenorth@localhost>
2012-06-04 19:48:16 +0100

    Cleanup: remove a file used only whilst developing truck compositing

--- src/pixel_generator/test_truck.py
+++
@@ -1,6 +0,0 @@
-import gestalts.fifth_wheel_mask_body as fifth_wheel_mask_body
-fifth_wheel_mask_body.generate('body_fifth_wheel_mask-blue_mask-2_8.png')
-
-import gestalts.truck as truck
-truck.generate('truck-hackler_R-chassis-tandem-7_8-cab-cc1-2_8-body_flat-cc1-5_8-cargo_coils-grey_metal.png')
-truck.generate('truck-hackler_R-chassis-tandem-4_8-cab-cc1-2_8-body_fifth_wheel_mask-blue_mask-2_8.png')
